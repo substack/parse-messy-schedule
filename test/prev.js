b9ca1b23aec502464a982c669359999a09cf03a7
James Halliday <mail@substack.net>
2015-12-21 12:34:00 -0500

    previous day test

--- test/prev.js
+++ test/prev.js
@@ -18,3 +18,21 @@ test('previous wednesday', function (t) {
   ])
   t.end()
 })
+
+test('previous every day', function (t) {
+  var str = 'every day at 7 pm'
+  var ev = parse(str, { created: new Date('2015-12-10 03:00') })
+  t.equal(ev.oneTime, false)
+  var n = '2015-12-10 03:00', outputs = []
+  for (var i = 0; i < 3; i++) {
+    n = ev.prev(n)
+    if (!n) break
+    outputs.push(n.toString().replace(/ GMT.*/, ''))
+  }
+  t.deepEqual(outputs, [
+    'Wed Dec 09 2015 19:00:00',
+    'Tue Dec 08 2015 19:00:00',
+    'Mon Dec 07 2015 19:00:00'
+  ])
+  t.end()
+})
